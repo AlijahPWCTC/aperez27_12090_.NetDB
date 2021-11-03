db8e583c897431b49f87d2cb85209bc2c003fe74
Alijah <aperez27@my.wctc.edu>
2021-11-03 16:19:25 -0600

    re

--- A4 Movie Library/testclass.cs
+++
@@ -1,9 +0,0 @@
-// using Xunit;
-// public class testclass{
-
-//methods did not work for testing
-// example test
-//  [Fact]
-//    public void PassingTest(){
-//      Assert.Equals(4, Program.exampleMethod(4, 6,7));
-//    }
